d97797f11c2f25e6f95334f10dea44568c1f993f
Vishal Kumar <vishalyadav6jd@gmail.com>
2025-08-27 15:09:58 +0530

    Update app file

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("Incoming request:", req.method, req.url);
+  next();
+});
+
 app.use("/api/v1", routes);
 
 app.get("/", (req, res) => {
